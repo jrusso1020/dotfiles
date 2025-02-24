d259d9006f54e9e5906d1c582571fcd36dc10d01
James Russo <jdrusso1020@gmail.com>
2025-02-24 10:37:07 -0800

    Update zsh.aliases.sh
    remove exa alias

--- aliases/zsh.aliases.sh
+++ aliases/zsh.aliases.sh
@@ -5,13 +5,5 @@ alias rl='source ~/.zshrc; echo ".zshrc reloaded"'
 # Main directories
 alias .f='cd ~/.dotfiles'
 
-if type exa > /dev/null 2>&1; then
-  alias ll='exa -alF --icons --color=always --group-directories-first'
-  alias llt='exa -alF --icons --color=always -s=mod --reverse'
-else
-  alias ll='ls -la'
-  alias llt='ls -lat'
-fi
-
 # Other bash stuff
 alias t="touch"
